7da247ded8b0ae0bf964ea53cd14d7a02ae068ee
weiliai <916880038@qq.com>
2020-02-23 21:54:11 +0800

    单例模式

---
+++ 2springDesignPattern/src/main/java/com/weiliai/pattern/sp/optimize/ContainerSingleton.java
@@ -0,0 +1,33 @@
+package com.weiliai.pattern.sp.optimize;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: Doug Li
+ * @Date 2020/2/23
+ * @Describe: 容器式单例.适用于实例非常多的情况,便于管理,非线程安全(不太懂,自认为线程安全)
+ */
+public class ContainerSingleton {
+
+    private ContainerSingleton(){};
+
+    private static Map<String,Object> ioc = new HashMap<String,Object>();
+
+    public static Object getBean(String className){
+        synchronized (ioc){
+            if(!ioc.containsKey(className)){
+                Object obj = null;
+                try{
+                    obj = Class.forName(className).newInstance();
+                    ioc.put(className,obj);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                return obj;
+            }else{
+                return ioc.get(className);
+            }
+        }
+    }
+}
---
+++ 2springDesignPattern/src/main/java/com/weiliai/pattern/sp/simple/SingletonTest.java
@@ -0,0 +1,37 @@
+package com.weiliai.pattern.sp.simple;
+
+import com.weiliai.pattern.sp.optimize.ThreadLocalSingleton;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Author: Doug Li
+ * @Date 2020/2/23
+ * @Describe: TODO
+ */
+public class SingletonTest {
+
+    public static final int num = 200;
+    public static void main(String[] args) {
+
+        ExecutorService service = Executors.newFixedThreadPool(num);
+        for (int i = 0; i < num; i++) {
+            service.execute(new ExecutorThread());
+        }
+        service.shutdown();
+        System.out.println("End");
+    }
+
+}
+class ExecutorThread implements Runnable{
+
+    @Override
+    public void run() {
+        //LazySingleton instance = LazySingleton.getInstance(); //线程不安全
+        //LazySingleton instance = LazySingleton.getInstance2();
+        //final Object instance = ContainerSingleton.getBean("com.weiliai.pattern.sp.simple.SingletonTest");
+        final ThreadLocalSingleton instance = ThreadLocalSingleton.getInstance();
+        System.out.println(Thread.currentThread().getName()+":"+instance);
+    }
+}
